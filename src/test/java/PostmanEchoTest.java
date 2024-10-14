fe6dfab35aaff9e78791d4917e941e93a53017d8
Альберт М. Галиуллин <Galiullin.A@progress.local>
2024-10-14 13:13:32 +0500

    Failed test PostmanEcho

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+public class PostmanEchoTest {
+    @Test
+    void shouldReturnSendData() {
+        String text = "Hello";
+        given()
+                .baseUri("https://postman-echo.com")
+                .body(text)
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", org.hamcrest.Matchers.equalTo("Hi"));
+    }
+}
